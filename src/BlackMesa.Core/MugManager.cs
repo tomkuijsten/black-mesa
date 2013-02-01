f79e1c7d4f2112c532b1f0c5aa8b9f4b0d551a4e
tomkuijsten <tomkuijsten@gmail.com>
2013-02-01 22:47:34 +0100

    First setup

---
+++ src/BlackMesa.Core/MugManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackMesa.Core
+{
+    /// <summary>
+    /// Manage the Mug
+    /// </summary>
+    class MugManager
+    {
+    }
+}
